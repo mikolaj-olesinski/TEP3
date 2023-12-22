08aeb2eec7499ade25ea7a3efcd4918864a8fa08
mikolajolesinski <mikolaj.olesinski11@gmail.com>
2023-12-22 14:54:15 +0100

    dodane pragma once zamiase ifdef

--- lab04/cTree/cTree.h
+++ lab04/cTree/cTree.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include "../cNode/cNode.h"
 #include <set>
--- lab04/main.cpp
+++ lab04/main.cpp
@@ -2,7 +2,12 @@
 
 int main() {
 
-    startUIAndDetermineTypeOfTree();
+    cTree<int> tree1, tree2, result;
+    tree1.enter({"+", "1", "2"});
+    tree2.enter({"+", "3", "4"});
+    result.printBT();
+
+    result = tree1 + tree2;
 
     return 0;
 }
